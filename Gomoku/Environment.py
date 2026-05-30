f6cddc12a4428f7a2c08de2d35adef7ababb9c62
Lukka <lukka.mayer@gmail.com>
2026-05-31 00:08:34 +0200

    Enhance Environment class with device support and GUI
    Refactor Environment class to include device checks and GUI setup.

--- Gomoku/Environment.py
+++ Gomoku/Environment.py
@@ -1,16 +1,95 @@
-
 from dataclasses import dataclass
 from dotenv import load_dotenv
+from pathlib import Path
+import tkinter as tk
+import warnings
+import torch
 import os
 
-load_dotenv()
+load_dotenv(Path(__file__).with_name("env.txt"))
 
 
 @dataclass(frozen=True)
 class EnvConfig:
     board_size_x: int
     board_size_y: int
-    WIN_LENGTH: int
+    win_length: int
 
     @classmethod
-    def from_env
+    def from_env(cls):
+        return cls(
+            board_size_x=int(os.environ["BOARD_SIZE_X"]),
+            board_size_y=int(os.environ["BOADR_SIZE_Y"]),
+            win_length=int(os.environ["WIN_LENGTH"])
+        )
+
+
+def quiet_device_check():
+    with warnings.catch_warnings(record=False):
+        warnings.filterwarnings(
+            action="ignore",
+            category=UserWarning
+        )
+        return torch.accelerator.is_available()
+
+
+class Environment:
+
+    def __init__(self):
+
+        self.config = EnvConfig.from_env()
+
+        device = "cpu"
+        if quiet_device_check():
+            self.device = torch.accelerator.current_accelerator()
+            print(f"You are using the {device} accelerator.")
+        else:
+            print("You are using your CPU.")
+
+        self.state_vector_X = torch.zeros(self.config.board_size_x*self.config.board_size_y).to(device)
+        self.state_vector_O = torch.zeros(self.config.board_size_x*self.config.board_size_y).to(device)
+
+        self.root = tk.Tk()
+        self.root.title("Gomoku")
+        self.current_player = "X"
+        self.buttons = []
+
+
+    def is_win(self):
+        return
+
+
+    def click_input(self, row, col):
+
+        current_player = self.current_player
+        button = self.buttons[row][col]
+
+        if button["text"] == "":
+            button["text"] = current_player
+            self.current_player = "O" if current_player == "X" else "X"
+
+    def visualizer(self):
+        for row in range(self.config.board_size_y):
+            button_row = []
+            for column in range(self.config.board_size_x):
+                btn = tk.Button(self.root,
+                                font=("Arial", 32),
+                                width=5,
+                                height=2,
+                                command=lambda r=row, c=column: self.click_input(r, c)
+                                )
+                btn.grid(row=row, column=column)
+                button_row.append(btn)
+            self.buttons.append(button_row)
+
+
+
+game = Environment()
+game.visualizer()
+game.root.mainloop()
+
+"""
+while not game.is_win():
+    game.visualizer()
+    game.click_input()
+"""
